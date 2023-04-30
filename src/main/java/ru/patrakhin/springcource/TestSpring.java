dcd3cdddd1d0ba03dfb3dbb0d45fb532c1d4283c
patrakhin <patrakhin@mail.ru>
2023-04-30 20:07:13 +0300

    8 lesson scope singleton and prototype

--- src/main/java/ru/patrakhin/springcource/TestSpring.java
+++ src/main/java/ru/patrakhin/springcource/TestSpring.java
@@ -10,14 +10,27 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+/*        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean comparsion = firstMusicPlayer==secondMusicPlayer;
+        System.out.println(comparsion);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());*/
+
+        /*System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());*/
 
 
         context.close();
